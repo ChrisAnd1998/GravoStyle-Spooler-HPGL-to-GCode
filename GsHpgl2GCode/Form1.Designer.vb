e952463c79d92553827c27e919d29a3f6c7b0c38
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-21 17:37:58 +0200

    Add files via upload

---
+++ GsHpgl2GCode/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.Red
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 43)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(187, 630)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.RichTextBox2.Location = New System.Drawing.Point(205, 43)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ReadOnly = True
+        Me.RichTextBox2.Size = New System.Drawing.Size(187, 630)
+        Me.RichTextBox2.TabIndex = 1
+        Me.RichTextBox2.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(523, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(62, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "20"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(424, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Z Axis Depth (mm)"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Convert"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(174, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Export"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.NotifyFilter = System.IO.NotifyFilters.FileName
+        Me.FileSystemWatcher1.Path = "C:\Windows\System32\spool\PRINTERS"
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 679)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(283, 11)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(302, 679)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(282, 11)
+        Me.ProgressBar2.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar2.TabIndex = 8
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox3.ForeColor = System.Drawing.Color.Lime
+        Me.RichTextBox3.Location = New System.Drawing.Point(398, 43)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.ReadOnly = True
+        Me.RichTextBox3.Size = New System.Drawing.Size(187, 630)
+        Me.RichTextBox3.TabIndex = 9
+        Me.RichTextBox3.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(596, 701)
+        Me.Controls.Add(Me.RichTextBox3)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "GravoStyle HPGL Spool > GCode v1.0"
+        Me.TopMost = True
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents RichTextBox3 As RichTextBox
+End Class
--- GsHpgl2GCode/Form1.vb
+++ GsHpgl2GCode/Form1.vb
@@ -1,1 +1,152 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
 
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        OpenFileDialog1.Filter = "GravoStyle HPGL Spooler files (*.spl)|*.spl|All files (*.*)|*.*"
+
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+        If result = Windows.Forms.DialogResult.OK Then
+            Dim path As String = OpenFileDialog1.FileName
+            Dim text As String = File.ReadAllText(path)
+            RichTextBox1.Text = text.ToString
+            ProgressBar1.Value = 0
+            ProgressBar2.Value = 0
+            RichTextBox2.Text = Nothing
+            RichTextBox3.Text = Nothing
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Convert()
+    End Sub
+
+    Sub Convert()
+        Button3.Enabled = False
+        ProgressBar1.Value = 0
+        ProgressBar2.Value = 0
+
+        RichTextBox2.Text = Nothing
+        RichTextBox3.Text = Nothing
+
+        RichTextBox2.AppendText("G0 Z0" & vbNewLine)
+
+        For Each line In RichTextBox1.Lines
+
+            If line.Contains("PU") AndAlso line.Contains(",") Then
+                RichTextBox2.AppendText(line.Replace("PU", "G0 X").Replace(",", " Y").Replace(";", "") & vbNewLine)
+            End If
+
+            If line.Contains("PD") AndAlso line.Contains(",") Then
+                RichTextBox2.AppendText(line.Replace("PD", "G0 X").Replace(",", " Y").Replace(";", "") & vbNewLine)
+            End If
+
+            If line.Contains("PU;") Then
+                RichTextBox2.AppendText("G0 Z0" & vbNewLine)
+            End If
+
+            If line.Contains("PD;") Then
+                RichTextBox2.AppendText("G0 Z" & TextBox1.Text & vbNewLine)
+            End If
+
+            ProgressBar1.Value = ProgressBar1.Value + 1
+
+        Next
+
+        System.Threading.Thread.Sleep(500) : Application.DoEvents()
+        For i As Integer = 0 To RichTextBox2.Lines.Count - 1
+
+            If RichTextBox2.Lines(i).Contains("X") Then
+                Dim strRegex2 As String = "\d+"
+                Dim myRegex2 As New Regex(strRegex2, RegexOptions.None)
+
+                Dim matches As New ArrayList
+
+                For Each myMatchx As Match In myRegex2.Matches(RichTextBox2.Lines(i).ToString)
+
+                    If myMatchx.Success Then
+                        If Not myMatchx.Value.ToString = "0" Then
+                            matches.Add(myMatchx.Value.ToString)
+                        End If
+                    End If
+                Next
+
+                RichTextBox3.AppendText(RichTextBox2.Lines(i).Replace(matches(1).ToString, (matches(1).ToString / 40).ToString.Replace(",", ".")).Replace(matches(0).ToString, (matches(0).ToString / 40)).Replace(",", ".").ToString & vbNewLine)
+            Else
+                RichTextBox3.AppendText(RichTextBox2.Lines(i).ToString & vbNewLine)
+            End If
+            ProgressBar2.Value = i
+        Next
+
+        Button3.Enabled = True
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SaveFileDialog1.Filter = "Gcode Files (*.gcode*)|*.gcode"
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK _
+         Then
+            My.Computer.FileSystem.WriteAllText _
+            (SaveFileDialog1.FileName, RichTextBox3.Text, True)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Shown
+        For Each arg As String In Environment.GetCommandLineArgs()
+            If arg.ToString.Contains(".SPL") Then
+                Dim text As String = File.ReadAllText(arg)
+                RichTextBox1.Text = text.ToString
+                ProgressBar1.Value = 0
+                ProgressBar2.Value = 0
+                RichTextBox2.Text = Nothing
+                RichTextBox3.Text = Nothing
+                Convert()
+            End If
+        Next arg
+    End Sub
+
+    Private Sub FileSystemWatcher1_Created(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher1.Created
+        RichTextBox1.Text = Nothing
+
+        ProgressBar1.Value = 0
+        ProgressBar2.Value = 0
+        RichTextBox2.Text = Nothing
+        RichTextBox3.Text = Nothing
+
+        System.Threading.Thread.Sleep(500) : Application.DoEvents()
+
+        If e.FullPath.Contains(".SPL") Then
+Read_Again:
+            Try
+                Dim text As String = File.ReadAllText(e.FullPath)
+                RichTextBox1.Text = text.ToString
+            Catch
+                GoTo Read_Again
+            End Try
+
+        End If
+
+        System.Threading.Thread.Sleep(500) : Application.DoEvents()
+
+        Try
+            My.Computer.FileSystem.DeleteFile(e.FullPath)
+        Catch
+
+        End Try
+
+        Convert()
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        ProgressBar1.Maximum = RichTextBox1.Lines.Count
+    End Sub
+
+    Private Sub RichTextBox2_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox2.TextChanged
+
+        ProgressBar2.Maximum = RichTextBox2.Lines.Count
+    End Sub
+
+End Class
