61d3d42a2e7d0252da2c071297b93dde15e41501
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-22 16:10:49 +0200

    Add files via upload

--- GsHpgl2GCode/Form1.Designer.vb
+++ GsHpgl2GCode/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
@@ -29,41 +29,46 @@ Partial Class Form1
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
         Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'RichTextBox1
         '
         Me.RichTextBox1.BackColor = System.Drawing.Color.Black
-        Me.RichTextBox1.ForeColor = System.Drawing.Color.Red
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.RichTextBox1.Location = New System.Drawing.Point(12, 43)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ReadOnly = True
-        Me.RichTextBox1.Size = New System.Drawing.Size(187, 630)
+        Me.RichTextBox1.Size = New System.Drawing.Size(187, 500)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = ""
         '
         'RichTextBox2
         '
         Me.RichTextBox2.BackColor = System.Drawing.Color.Black
-        Me.RichTextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.RichTextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.RichTextBox2.Location = New System.Drawing.Point(205, 43)
         Me.RichTextBox2.Name = "RichTextBox2"
         Me.RichTextBox2.ReadOnly = True
-        Me.RichTextBox2.Size = New System.Drawing.Size(187, 630)
+        Me.RichTextBox2.Size = New System.Drawing.Size(187, 500)
         Me.RichTextBox2.TabIndex = 1
         Me.RichTextBox2.Text = ""
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Location = New System.Drawing.Point(12, 10)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
@@ -76,7 +81,7 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(523, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(1107, 12)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(62, 20)
         Me.TextBox1.TabIndex = 3
@@ -85,25 +90,16 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(424, 15)
+        Me.Label1.Location = New System.Drawing.Point(1008, 15)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(93, 13)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Z Axis Depth (mm)"
         '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(93, 12)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Convert"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
         'Button3
         '
         Me.Button3.Enabled = False
-        Me.Button3.Location = New System.Drawing.Point(174, 12)
+        Me.Button3.Location = New System.Drawing.Point(1175, 10)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 23)
         Me.Button3.TabIndex = 6
@@ -119,41 +115,73 @@ Partial Class Form1
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(12, 679)
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 549)
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(283, 11)
+        Me.ProgressBar1.Size = New System.Drawing.Size(379, 19)
         Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
         Me.ProgressBar1.TabIndex = 7
         '
         'ProgressBar2
         '
-        Me.ProgressBar2.Location = New System.Drawing.Point(302, 679)
+        Me.ProgressBar2.Location = New System.Drawing.Point(398, 549)
         Me.ProgressBar2.Name = "ProgressBar2"
-        Me.ProgressBar2.Size = New System.Drawing.Size(282, 11)
+        Me.ProgressBar2.Size = New System.Drawing.Size(852, 19)
         Me.ProgressBar2.Style = System.Windows.Forms.ProgressBarStyle.Continuous
         Me.ProgressBar2.TabIndex = 8
         '
         'RichTextBox3
         '
         Me.RichTextBox3.BackColor = System.Drawing.Color.Black
-        Me.RichTextBox3.ForeColor = System.Drawing.Color.Lime
+        Me.RichTextBox3.ForeColor = System.Drawing.Color.LimeGreen
         Me.RichTextBox3.Location = New System.Drawing.Point(398, 43)
         Me.RichTextBox3.Name = "RichTextBox3"
         Me.RichTextBox3.ReadOnly = True
-        Me.RichTextBox3.Size = New System.Drawing.Size(187, 630)
+        Me.RichTextBox3.Size = New System.Drawing.Size(187, 500)
         Me.RichTextBox3.TabIndex = 9
         Me.RichTextBox3.Text = ""
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(591, 43)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(660, 500)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(595, 426)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(9, 112)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(595, 529)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(112, 9)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox3.TabIndex = 12
+        Me.PictureBox3.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(596, 701)
+        Me.ClientSize = New System.Drawing.Size(1262, 582)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.RichTextBox3)
         Me.Controls.Add(Me.ProgressBar2)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
@@ -166,6 +194,9 @@ Partial Class Form1
         Me.Text = "GravoStyle HPGL Spool > GCode v1.0"
         Me.TopMost = True
         CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -177,11 +208,13 @@ Partial Class Form1
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
     Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
     Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents ProgressBar2 As ProgressBar
     Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
--- GsHpgl2GCode/Form1.vb
+++ GsHpgl2GCode/Form1.vb
@@ -2,6 +2,26 @@
 Imports System.Text.RegularExpressions
 
 Public Class Form1
+    Dim xold As Integer
+    Dim yold As Integer
+
+    Dim x1 As Integer
+    Dim y1 As Integer
+    Dim x2 As Integer
+    Dim y2 As Integer
+
+    Dim blackpen As New Drawing.Pen(Color.Yellow)
+
+    Dim counts1x As String()
+    Dim counts1y As String()
+
+    Sub draw()
+        Dim g As Graphics
+        blackpen.Color = Color.Cyan
+        g = PictureBox1.CreateGraphics
+
+        g.DrawLine(blackpen, x1, y1, x2, y2)
+    End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
@@ -12,16 +32,18 @@ Public Class Form1
         If result = Windows.Forms.DialogResult.OK Then
             Dim path As String = OpenFileDialog1.FileName
             Dim text As String = File.ReadAllText(path)
-            RichTextBox1.Text = text.ToString
-            ProgressBar1.Value = 0
-            ProgressBar2.Value = 0
-            RichTextBox2.Text = Nothing
-            RichTextBox3.Text = Nothing
-        End If
-    End Sub
+            Dim first As String = System.IO.File.ReadAllLines(path)(0)
+            If first.ToString.Contains("IN;PA;") Then
+                RichTextBox1.Text = text.ToString
+                ProgressBar1.Value = 0
+                ProgressBar2.Value = 0
+                RichTextBox2.Text = Nothing
+                RichTextBox3.Text = Nothing
+                PictureBox1.Image = Nothing
+                Convert()
+            End If
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Convert()
+        End If
     End Sub
 
     Sub Convert()
@@ -57,6 +79,9 @@ Public Class Form1
         Next
 
         System.Threading.Thread.Sleep(500) : Application.DoEvents()
+
+        Dim bmp As Bitmap = New Drawing.Bitmap(PictureBox1.Width, PictureBox1.Height)
+
         For i As Integer = 0 To RichTextBox2.Lines.Count - 1
 
             If RichTextBox2.Lines(i).Contains("X") Then
@@ -74,12 +99,39 @@ Public Class Form1
                     End If
                 Next
 
-                RichTextBox3.AppendText(RichTextBox2.Lines(i).Replace(matches(1).ToString, (matches(1).ToString / 40).ToString.Replace(",", ".")).Replace(matches(0).ToString, (matches(0).ToString / 40)).Replace(",", ".").ToString & vbNewLine)
+                Dim newstr = RichTextBox2.Lines(i).Replace(matches(1).ToString, (matches(1).ToString / 40).ToString.Replace(",", ".")).Replace(matches(0).ToString, (matches(0).ToString / 40)).Replace(",", ".").ToString
+
+                RichTextBox3.AppendText(newstr & vbNewLine)
+
+                Dim lineclean As String = newstr.Replace("G0 X", "").Replace("Y", "")
+                Dim counts As String() = lineclean.Split(New Char() {" "c})
+
+                counts1x = counts(0).Split(New Char() {"."c})
+                counts1y = counts(1).Split(New Char() {"."c})
+
+                x1 = xold
+                y1 = yold
+                x2 = counts1x(0)
+                y2 = counts1y(0)
+
+                xold = x2
+                yold = y2
+
+                draw()
             Else
                 RichTextBox3.AppendText(RichTextBox2.Lines(i).ToString & vbNewLine)
             End If
             ProgressBar2.Value = i
         Next
+
+        ' PictureBox1.Image.Save("myimage.jpg", Drawing.Imaging.ImageFormat.Jpeg)
+
+        Dim s As Size = PictureBox1.Size
+        Dim memoryImage = New Bitmap(s.Width, s.Height)
+        Dim memoryGraphics As Graphics = Graphics.FromImage(memoryImage)
+        Dim ScreenPos As Point = Me.PictureBox1.PointToScreen(New Point(0, 0))
+        memoryGraphics.CopyFromScreen(ScreenPos.X, ScreenPos.Y, 0, 0, s)
+        PictureBox1.Image = memoryImage
 
         Button3.Enabled = True
     End Sub
@@ -88,6 +140,7 @@ Public Class Form1
         SaveFileDialog1.Filter = "Gcode Files (*.gcode*)|*.gcode"
         If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK _
          Then
+
             My.Computer.FileSystem.WriteAllText _
             (SaveFileDialog1.FileName, RichTextBox3.Text, True)
         End If
@@ -97,12 +150,18 @@ Public Class Form1
         For Each arg As String In Environment.GetCommandLineArgs()
             If arg.ToString.Contains(".SPL") Then
                 Dim text As String = File.ReadAllText(arg)
-                RichTextBox1.Text = text.ToString
-                ProgressBar1.Value = 0
-                ProgressBar2.Value = 0
-                RichTextBox2.Text = Nothing
-                RichTextBox3.Text = Nothing
-                Convert()
+
+                Dim first As String = System.IO.File.ReadAllLines(arg)(0)
+                If first.ToString.Contains("IN;PA;") Then
+                    RichTextBox1.Text = text.ToString
+                    ProgressBar1.Value = 0
+                    ProgressBar2.Value = 0
+                    RichTextBox2.Text = Nothing
+                    RichTextBox3.Text = Nothing
+                    PictureBox1.Image = Nothing
+                    Convert()
+                End If
+
             End If
         Next arg
     End Sub
@@ -114,29 +173,39 @@ Public Class Form1
         ProgressBar2.Value = 0
         RichTextBox2.Text = Nothing
         RichTextBox3.Text = Nothing
+        PictureBox1.Image = Nothing
 
         System.Threading.Thread.Sleep(500) : Application.DoEvents()
+        Try
 
-        If e.FullPath.Contains(".SPL") Then
+            Dim first As String = System.IO.File.ReadAllLines(e.FullPath)(0)
+
+            If e.FullPath.Contains(".SPL") Then
 Read_Again:
+                Try
+                    Dim text As String = File.ReadAllText(e.FullPath)
+
+                    If first.ToString.Contains("IN;PA;") Then
+                        RichTextBox1.Text = text.ToString
+                    End If
+                Catch
+                    GoTo Read_Again
+                End Try
+
+            End If
+
+            System.Threading.Thread.Sleep(500) : Application.DoEvents()
+
             Try
-                Dim text As String = File.ReadAllText(e.FullPath)
-                RichTextBox1.Text = text.ToString
+                My.Computer.FileSystem.DeleteFile(e.FullPath)
             Catch
-                GoTo Read_Again
+
             End Try
 
-        End If
-
-        System.Threading.Thread.Sleep(500) : Application.DoEvents()
-
-        Try
-            My.Computer.FileSystem.DeleteFile(e.FullPath)
+            Convert()
         Catch
 
         End Try
-
-        Convert()
 
     End Sub
 
